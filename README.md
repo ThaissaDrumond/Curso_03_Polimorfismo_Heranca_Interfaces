fae3d78b718856c4a63a5f48f932cdbada217177
ThaissaDrumond <thaissa.villaverde@gmail.com>
2022-10-30 15:05:20 -0300

    Aula 01: Introdução a herança

---
+++ README.md
@@ -0,0 +1,15 @@
+# ****JAVA POLIMORFISMO: ENTENDA HERANÇA E INTERFACES****
+
+#### Repositório criado para acompanhamento de evolução do código durante aulas do curso de Java Polimorfismo
+
+###### Conteúdo das aulas:
+
+
+
+- Introdução a herança
+- Super e reescrita de métodos
+- Entendendo Polimorfismo
+- Herança e o uso construtores
+- Classes e métodos abstratos
+- Interfaces
+- Praticando herança e interfaces
---
+++ byteebank-herdado/src/Gerente.java
@@ -0,0 +1,21 @@
+//Gerente é um Funcionário, Gerente herda da classe Funcionário
+
+public class Gerente extends Funcionario{
+    private int senha;
+
+    public boolean autentica(int senha) {
+        if(this.senha == senha) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    /*public double getBonificacao() {
+        return this.salario;
+    }*/
+}
